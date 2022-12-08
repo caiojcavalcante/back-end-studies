9dd00e76aa13dbfeca8e39c6071c8bafb038709a
Caio Cavalcante <danielcaio75@gmail.com>
2022-12-07 21:36:22 -0300

    create project

---
+++ django/social_book/social_book/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('upload', views.index, name='upload'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
+    path('settings', views.settings, name='settings'),
+]
---
+++ django/social_book/social_book/core/views.py
@@ -0,0 +1,110 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Post, Profile
+from django.http import HttpResponse
+
+# Create your views here.
+@login_required(login_url='signin')
+def index(request):
+
+    user_object = User.objects.get(username=request.user)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+
+                # Log user in and redirect to settings page
+
+                user_login = auth.authenticate(username=username, password=password1)
+                auth.login(request, user_login)
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
+                
+
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+
+    return render(request, 'signup.html')
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('signin')
+
+    return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def settings(request):
+
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+
+        else:
+            image = request.FILES['image']
+
+        bio = request.POST['bio']
+        location = request.POST['location']
+        user_profile.profileimg = image
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+
+        return redirect('settings')
+
+    return render(request, 'settings.html', {'user_profile': user_profile})
+
+@login_required(login_url='signin')
+def logout(request):
+    
+    auth.logout(request)
+    return redirect('signin')
+
+@login_required(login_url='signin')
+def upload(request):
+
+    if request.method == 'POST':
+        HttpResponse("criando um post")
+        user = request.user.username
+        # image = request.FILES.get('image_upload')
+        # caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user)
+        new_post.save()
+
+    return redirect('/')
